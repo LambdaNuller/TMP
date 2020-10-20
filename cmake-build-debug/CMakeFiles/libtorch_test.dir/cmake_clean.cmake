8438b95c7bc955bff21886389df402f81965b5cf
nuller-cv <nuller26@foxmail.com>
2020-10-20 10:48:27 +0800

    first commit test using clion

---
+++ cmake-build-debug/CMakeFiles/libtorch_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libtorch_test.dir/main.cpp.o"
+  "libtorch_test"
+  "libtorch_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libtorch_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
